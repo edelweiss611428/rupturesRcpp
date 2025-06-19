b49d8f2c6581452908141b7d7cc4106a36255a39
edelweiss611428 <edelweiss611428@gmail.com>
2025-06-19 17:09:29 +0800

    Update documentation for PeltL2

--- R/PeltR6.R
+++ R/PeltR6.R
@@ -1,7 +1,22 @@
+#' PeltL2
+#'
+#' An R6 class implements Pelt for offline changepoint detection,
+#' currently only supports the L2 cost function.
+#'
+#' @docType class
+#'
 #' @importFrom R6 R6Class
 #' @export
-
-library("R6")
+#'
+#' @field tsMat A time series matrix of size $n \times p$ where
+#' where each row is an observation ordered in time.
+#' @field minSize An integer specifying the minimum size of a segment; by default, minSize = 1.
+#' @field jump  An integer value defines the step size for the search grid during changepoint
+#' detection -  only candidates changepoints \{1,k+1,2k+1,...\}  will be considered.
+#' @field fitted  A boolean value specifies whether or not PeltL2 has been fitted on a dataset
+#'- otherwise methods like $plot() and $predict() will return an error; by default, fitted = FALSE.
+#' @field n The number of observations.
+#' @field p The number of dimensions.
 
 PeltL2 <- R6Class(
   "PeltL2",
@@ -15,12 +30,24 @@ PeltL2 <- R6Class(
     n = NULL,
     p = NULL,
 
+    #' Initialise a PeltL2 object
+    #' @param minSize An integer specifying the minimum regime size - is 1 by default.
+    #' @param jump <int> An integer determines the search space - only changepoints
+    #'  \{1,k+1,2k+1,...\}  will be considered; by default jump = 1.
+    #' @return Return no value but initialise a PeltL2 object with minSize and jump
+    #' parameters.
+    #
     initialize = function(minSize = 1, jump = 1) {
       self$minSize = minSize
       self$jump = jump
       print("You have created a PeltL2 object!")
     },
 
+    #' Input a time series matrix for PeltL2 changepoint detection
+    #' @param tsMat A time series matrix of size $n \times p$ where
+    #' where each row is an observation ordered in time.
+    #' @return Return no value but input tsMat, n, and p, and fitted = TRUE.
+    #'
     fit = function(tsMat) { #Add error handling for tsMat
       self$tsMat = tsMat
       self$n = nrow(tsMat)
@@ -28,7 +55,12 @@ PeltL2 <- R6Class(
       self$fitted = TRUE
     },
 
-    predict = function(pen){
+    #' Carry out change-point detection for a specified penalty - must be run after fitted!
+    #' @param pen A non-negative double specifying a linear penaltu value for each additional
+    #' change-point.
+    #' @return Return no value but input tsMat, n, and p, and fitted = TRUE.
+    #'
+    predict = function(pen = 0){
       if(!self$fitted){
         stop("Must run $fit() before running $predict()!!!")
       }
@@ -41,6 +73,12 @@ PeltL2 <- R6Class(
       return(endPts[-nEndPts]) #Remove n
     },
 
+    #' Plot PeltL2 segmentation results for a specified dimension - must be run after fitted
+    #' @param pen A non-negative double specifying a linear penaltu value for each additional
+    #' change-point.
+    #'
+    #' @return Return no value but input tsMat, n, and p, and fitted = TRUE.
+    #'
     plot = function(pen, d = 1, xlab = "Iteration", ylab = "Value", #Add error handling for (pen, d)
                     main = paste0("peltSeg with pen = ", round(pen,2))){
 
@@ -67,5 +105,6 @@ PeltL2 <- R6Class(
     }
 
   ),
+
 )
 
