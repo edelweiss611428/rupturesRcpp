af74770e9a3c00a9994aa0f93e8efb2828d72b63
edelweiss611428 <edelweiss611428@gmail.com>
2025-06-19 17:02:27 +0800

    Remove /deprecated and /tmp
    Files in tmp have been integrated to the repo - maybe some changes are needed to ensure consistency (e.g. either arma::vec or arma::NumericVector)

--- tmp/peltL2.h
+++
@@ -1,9 +0,0 @@
-#ifndef PELTL2_H
-#define PELTL2_H
-
-#include <vector>
-#include <armadillo>
-
-std::vector<int> peltL2(const arma::mat& signal, double penalty, int minSize, int jump);
-
-#endif //PELTL2_H
