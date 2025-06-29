83775fa7bbcdb949d1c4af7c8a544451df6d5e96
edelweiss611428 <edelweiss611428@gmail.com>
2025-06-30 05:50:58 +1000

    Fix bug in Cost_VAR (p, nr- 1) longest interval
    binSeg and PELT assume that intervals are of the form (start, End)
    where max(End) = nr and start = 0 - count from 1 cuz 0 is excluded.
    
    However, for cost_var, the longest interval is (p, nr-1) - here counted from 0.
    
    Therefore some adjustments needed to be made, particularly
    
    if(start < p){ //to account for the fact that the longest valid section is y[p:(nr-1)] - here idx from 0
        start = p;
      }
    
      end = end - 1;
    and
      arma::mat Z_sub = Z_full.rows(start - p, end-p);
      arma::mat Y_sub = X.rows(start, end);

--- src/Cost_VAR.cpp
+++ src/Cost_VAR.cpp
@@ -13,33 +13,50 @@ Cost_VAR::Cost_VAR(const arma::mat& tsMat, const int& pVAR)  : p(pVAR), X(tsMat)
   Z_full.set_size(nr-p, J);
   Z_full.col(0).ones();
 
-  for (int L = 0; L < p; L++) { //L for lag
-    Z_full.cols(1 + L * nc, (L + 1) * nc) = X.rows(p - L - 1, nr - L - 1);
+  for (int L = 0; L < p; L++) {
+    Z_full.cols(1 + L * nc, (L + 1) * nc) = X.rows(p - L - 1, nr - L - 2);
   }
+
+
 };
 
 double Cost_VAR::effEvalCpp(int start, int end,
                             bool addSmallDiag,      // ignored
                             double epsilon) const {  // ignored
 
+
+  if(start < p){ //to account for the fact that the longest valid section is y[p:(nr-1)] - here idx from 0
+    start = p;
+  }
+
+  end = end - 1;
+
   int len = end - start;
   if(len < J){
     return 0; //in case numerical failure will occur, return 0
   }
 
-  int segStart = start + 1;
 
-
-  if (segStart - p < 0) {
-    return 0; //in case failure will occur, return 0
+  arma::mat Z_sub = Z_full.rows(start - p, end-p);
+  arma::mat Y_sub = X.rows(start, end);
+  arma::mat regCoefs;
+  try {
+    regCoefs = arma::solve(Z_sub, Y_sub);
+  } catch (...) {
+    return 0;  //in case failure will occur, return 0
   }
-
-  arma::mat Z_sub = Z_full.rows(segStart - p, end-p);
-  arma::mat Y_sub = X.rows(start+1, end);
-
-  arma::mat regCoefs = arma::solve(Z_sub, Y_sub);
   arma::mat errMat = Y_sub - Z_sub * regCoefs;
 
   return arma::accu(arma::square(errMat));
 
+}
+
+
+RCPP_MODULE(CostVAR_module) {
+  class_<Cost_VAR>("CostVAR")
+
+  .constructor<arma::mat, int>()
+
+  .method("effEvalCpp", &Cost_VAR::effEvalCpp,
+  "Evaluate VAR cost on interval (start, end]");
 }
