72bdf87193fc89cb196d3dfe32beb33f95b0a570
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-02 09:02:51 +1000

    Add documentation for Cost modules in R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,23 @@
-#' @export Cost_L2
-Rcpp::loadModule("Cost_L2_module", TRUE)
-#' @export Cost_VAR
-Rcpp::loadModule("Cost_VAR_module", TRUE)
-#' @export Cost_SIGMA
-Rcpp::loadModule("Cost_SIGMA_module", TRUE)
+#' @title Cost_L2 module
+#' @description Load Rcpp module for Cost_L2
+#' @importFrom Rcpp loadModule
+#' @import methods
+#' @export
+#' @name Cost_L2
+loadModule("Cost_L2_module", TRUE)
 
+#' @title Cost_VAR module
+#' @description Load Rcpp module for Cost_VAR
+#' @importFrom Rcpp loadModule
+#' @import methods
+#' @export
+#' @name Cost_VAR
+loadModule("Cost_VAR_module", TRUE)
+
+#' @title Cost_SIGMA module
+#' @description Load Rcpp module for Cost_SIGMA
+#' @importFrom Rcpp loadModule
+#' @import methods
+#' @export
+#' @name Cost_SIGMA
+loadModule("Cost_SIGMA_module", TRUE)
