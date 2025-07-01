4b7bae7e56cba3264c260e9ff7a98d43eece039a
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-02 08:14:34 +1000

    Create module exporter in R for L2, VAR, SIGMA

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+#' @export Cost_L2
+Rcpp::loadModule("Cost_L2_module", TRUE)
+#' @export Cost_VAR
+Rcpp::loadModule("Cost_VAR_module", TRUE)
+#' @export Cost_SIGMA
+Rcpp::loadModule("Cost_SIGMA_module", TRUE)
+
