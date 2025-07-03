c0520bdc241d82b5c9e37f5125ba035d255bb9a8
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-04 00:46:50 +1000

    Add .onAttach ASCII art

--- R/zzz.R
+++ R/zzz.R
@@ -41,3 +41,22 @@ loadModule("Cost_VAR_module", TRUE)
 #' @export
 #' @name Cost_SIGMA
 loadModule("Cost_SIGMA_module", TRUE)
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(r"(
+
++--------------------------------------------------------------------+
+|                                                                    |
+|                    _                       ____                    |
+|   _ __ _   _ _ __ | |_ _   _ _ __ ___  ___|  _ \ ___ _ __  _ __    |
+|  | '__| | | | '_ \| __| | | | '__/ _ \/ __| |_) / __| '_ \| '_ \   |
+|  | |  | |_| | |_) | |_| |_| | | |  __/\__ \  _ < (__| |_) | |_) |  |
+|  |_|   \__,_| .__/ \__|\__,_|_|  \___||___/_| \_\___| .__/| .__/   |
+|             |_|                                     |_|   |_|      |
+|                                               version 0.1.0        |
++--------------------------------------------------------------------+
+
+See https://github.com/edelweiss611428/rupturesRcpp/tree/main/README.md
+for basic usage!
+)") }
+
