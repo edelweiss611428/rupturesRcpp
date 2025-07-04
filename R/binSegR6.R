835985afbc1d5af82324a0429d52a6c6c9919c36
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-05 05:22:29 +1000

    Update documentation for binSeg, PELT, and cost modules regarding latest changes
    i.e.,
    SIGMA returns the most negative double if failed
    +
    new O(1) or maybe approximately VAR module

--- R/binSegR6.R
+++ R/binSegR6.R
@@ -304,20 +304,19 @@ binSeg = R6Class(
     #'
     #' - **L2 cost function**:
     #' \deqn{c_{L_2}(y_{(a+1)...b}) := \sum_{t = a+1}^{b} \| y_t - \bar{y}_{(a+1)...b} \|_2^2}
-    #' where \eqn{\bar{y}_{(a+1)...b}} is the empirical mean of the segment. If
-    #' `a+1 = b`, return 0.
+    #' where \eqn{\bar{y}_{(a+1)...b}} is the empirical mean of the segment. If \eqn{a \ge b - 1}, return 0.
     #'
     #' - **SIGMA cost function**:
     #' \deqn{c_{\sum}(y_{(a+1)...b}) := (b - a)\log \det \hat{\Sigma}_{(a+1)...b}} where \eqn{\hat{\Sigma}_{(a+1)...b}} is
     #' the empirical covariance matrix of the segment without Bessel's correction. Here, if `addSmallDiag = TRUE`, a small
     #' bias `epsilon` is added to the diagonal of estimated covariance matrices to improve numerical stability. If
-    #' \eqn{\hat{\Sigma}} is singular, return 0. If `a+1 = b`, return 0.
+    #' \eqn{\hat{\Sigma}} or \eqn{a \ge b - 1}, return the most negative double.
     #'
     #' - **VAR(r) cost function**:
     #' \deqn{c_{\mathrm{VAR}}(y_{(a+1)...b}) := \sum_{t = a+r+1}^{b} \left\| y_t - \sum_{j=1}^r \hat A_j y_{t-j} \right\|_2^2}
     #' where \eqn{\hat A_j} are the estimated VAR coefficients, commonly estimated via the OLS criterion. An approximate linear
-    #' solver will be used when exact `arma::solve()` fails. If no solution found, return 0. If `a-b < p*r+1` (i.e., not enough observations),
-    #' return 0.
+    #' solver will be used when exact `arma::solve()` fails. If no solution found, \eqn{a-b < p*r+1} (i.e., not enough
+    #' observations), or \eqn{a \ge n-p} (where `n` is the time series length), return 0.
     eval = function(a, b){
 
       if(!private$.fitted){
--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,7 @@
 #' @details
 #' **L2 cost function**:
 #' \deqn{c_{L_2}(y_{(a+1)...b}) := \sum_{t = a+1}^{b} \| y_t - \bar{y}_{(a+1)...b} \|_2^2}
-#' where \eqn{\bar{y}_{(a+1)...b}} is the empirical mean of the segment. If
-#' `a+1 = b`, return 0.
+#' where \eqn{\bar{y}_{(a+1)...b}} is the empirical mean of the segment. If \eqn{a \ge b - 1}, return 0
 #'
 #' @export
 #' @name Cost_L2
@@ -20,8 +19,8 @@ loadModule("Cost_L2_module", TRUE)
 #' **VAR cost function**:
 #' \deqn{c_{\mathrm{VAR}}(y_{(a+1)...b}) := \sum_{t = a+r+1}^{b} \left\| y_t - \sum_{j=1}^r \hat A_j y_{t-j} \right\|_2^2}
 #' where \eqn{\hat A_j} are the estimated VAR coefficients, commonly estimated via the OLS criterion. An approximate linear
-#' solver will be used when exact `arma::solve()` fails. If no solution found, return 0. If `a-b < p*r+1` (i.e., not enough observations),
-#' return 0.
+#' solver will be used when exact `arma::solve()` fails. If no solution found, \eqn{a-b < p*r+1} (i.e., not enough
+#' observations), or \eqn{a \ge n-p} (where `n` is the time series length), return 0.
 #'
 #' @export
 #' @name Cost_VAR
@@ -33,10 +32,10 @@ loadModule("Cost_VAR_module", TRUE)
 #' @import methods
 #' @details
 #' **SIGMA cost function**:
-#' \deqn{c_{\sum}(y_{(a+1)...b}) := (b - a)\log \det \hat{\Sigma}_{(a+1)...b}} where \eqn{\hat{\Sigma}_{(a+1)..b}} is
+#' \deqn{c_{\sum}(y_{(a+1)...b}) := (b - a)\log \det \hat{\Sigma}_{(a+1)...b}} where \eqn{\hat{\Sigma}_{(a+1)...b}} is
 #' the empirical covariance matrix of the segment without Bessel's correction. Here, if `addSmallDiag = TRUE`, a small
 #' bias `epsilon` is added to the diagonal of estimated covariance matrices to improve numerical stability. If
-#' \eqn{\hat{\Sigma}} is singular, return 0. If `a+1 = b`, return 0.
+#' \eqn{\hat{\Sigma}} or \eqn{a \ge b - 1}, return the most negative double.
 #'
 #' @export
 #' @name Cost_SIGMA
