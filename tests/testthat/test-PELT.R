e892b65451c20d411bc0453b0470aa69cd91c566
edelweiss611428 <edelweiss611428@gmail.com>
2025-07-07 08:21:56 +1000

    Update test cases for Pelt

--- tests/testthat/test-PELT.R
+++ tests/testthat/test-PELT.R
@@ -4,16 +4,21 @@ test_that("PELT with L2/SIGMA/VAR works for constant segments", {
 
   tsMat = matrix(c(rep(0,50), rep(5, 50), rep(10, 50)))
 
-  costFunc = c("L2", "SIGMA", "VAR")
-  for(i in 1:3){
-
-    costFuncObj = createCostFunc(costFunc[i])
-    PeltObj = PELT$new(costFuncObj = costFuncObj)
-    PeltObj$fit(tsMat)
-    expect_equal( PeltObj$predict(pen = 0.1), seq(50,150,50))
-
-  }
-
-
+  #L2
+  costFuncObj = createCostFunc("L2")
+  PeltObj = PELT$new(costFuncObj = costFuncObj)
+  PeltObj$fit(tsMat)
+  expect_equal( PeltObj$predict(pen = 0.1), seq(50,150,50))
+  #SIGMA
+  costFuncObj = createCostFunc("SIGMA")
+  PeltObj = PELT$new(costFuncObj = costFuncObj)
+  PeltObj$fit(tsMat)
+  expect_equal( PeltObj$predict(pen = 0.1), seq(50,150,50))
+  #VAR
+  costFuncObj = createCostFunc("VAR")
+  PeltObj = PELT$new(costFuncObj = costFuncObj)
+  PeltObj$fit(tsMat)
+  expect_equal( PeltObj$predict(pen = 0.1), seq(50,150,50))
+  expect_false(PeltObj$eval(0,51) == 0)
+  expect_true(all.equal(PeltObj$eval(0,50), 0))
 })
-
